8301007dd22fcb7ecd1509c1de6970e428a5126d
Mohammad Rahimi <moyi.pary@gmail.com>
2024-11-19 21:34:38 +0330

    prop and state for list group component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import ListGroup from "./componenets/ListGroup";
 function App() {
   const items: string[] = ["New York", "Qom", "Tehran", "Tokyo"];
   
-  return <div><ListGroup items={items} heading={"Cities"} /></div>
+  const handleSelectItem = (item: string) => {
+    console.log(item)
+  }
+  
+  return <div><ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem} /></div>
 }
 
 export default App;
--- src/componenets/ListGroup.tsx
+++ src/componenets/ListGroup.tsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  onSelectItem: (item: string) => void;
 }
 
-function ListGroup({ items, heading }: Props) {
+function ListGroup({ items, heading, onSelectItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
   const getMessage = () => {
     return items.length === 0 && <p>No item found!</p>;
@@ -21,6 +22,7 @@ function ListGroup({ items, heading }: Props) {
             key={item}
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item)
             }}
             className={
               selectedIndex === index
