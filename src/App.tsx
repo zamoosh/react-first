7244d8a4bff067dcab3f83fe99b63b5594cad98a
Mohammad Rahimi <moyi.pary@gmail.com>
2024-11-20 21:00:20 +0330

    update array in useState

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,38 @@ import { useState } from "react";
 import Heart from "./componenets/HeartComponent/Heart";
 
 function App() {
-  const [customer, setCustomer] = useState({
-    name: "Americano",
-    address: {
-      city: "Qom",
-      zipCode: "911444",
-    },
-  });
+  const [tags, setTags] = useState(["happy", "cheerful"]);
 
-  const handleClick = () => {
-    setCustomer({
-      ...customer,
-      address: { ...customer.address, zipCode: "911888" },
-    });
+  const handleClickAdd = () => {
+    setTags([...tags, "mohammad"]);
+  };
+
+  const handleClickRemove = () => {
+    // setTags(tags.filter((tag) => tag !== "cheerful"));
+    const newTags: string[] = [];
+    for (let i = 0; i < tags.length - 1; i++) {
+      newTags.push(tags[i])
+    }
+    setTags(newTags);
+  };
+
+  const handleClickUpdate = () => {
+    setTags([...tags].map((tag) => (tag === "happy" ? "sadness" : tag)));
   };
 
   return (
     <div className={"container mt-3"}>
-      {customer.address.zipCode}
-      <button onClick={handleClick}>click to update customer</button>
+      <pre>{tags}</pre>
+      
+      <button onClick={handleClickAdd}>add</button>
+
+      <br />
+
+      <button onClick={handleClickRemove}>remove</button>
+
+      <br />
+
+      <button onClick={handleClickUpdate}>update</button>
 
       <Heart onClick={() => console.log("heart clicked")} />
     </div>
