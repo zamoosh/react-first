0bc45994a8aba179b4c6ce1c4d60ca908555dce2
Mohammad Rahimi <moyi.pary@gmail.com>
2024-11-20 21:13:59 +0330

    update array of objects using useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,26 @@
 import { useState } from "react";
 import Heart from "./componenets/HeartComponent/Heart";
+import { nanoid } from "nanoid";
 
 function App() {
-  const [tags, setTags] = useState(["happy", "cheerful"]);
+  const [bugs, setBugs] = useState([
+    { id: 1, name: "bug 1", fixed: false },
+    { id: 2, name: "bug 2", fixed: false },
+  ]);
 
-  const handleClickAdd = () => {
-    setTags([...tags, "mohammad"]);
-  };
-
-  const handleClickRemove = () => {
-    // setTags(tags.filter((tag) => tag !== "cheerful"));
-    const newTags: string[] = [];
-    for (let i = 0; i < tags.length - 1; i++) {
-      newTags.push(tags[i]);
-    }
-    setTags(newTags);
-  };
-
-  const handleClickUpdate = () => {
-    setTags([...tags].map((tag) => (tag === "happy" ? "sadness" : tag)));
+  const handleClick = () => {
+    setBugs(
+      [...bugs].map((bug) => (bug.id === 2 ? { ...bug, fixed: true } : bug)),
+    );
   };
 
   return (
     <div className={"container mt-3"}>
-      <pre>{tags}</pre>
+      {bugs.map((elem) => (
+        <div key={nanoid()}>{elem.name} {elem.fixed ? "true" : "false"}</div>
+      ))}
 
-      <button onClick={handleClickAdd}>add</button>
-
-      <br />
-
-      <button onClick={handleClickRemove}>remove</button>
-
-      <br />
-
-      <button onClick={handleClickUpdate}>update</button>
+      <button onClick={handleClick}>click me</button>
 
       <Heart onClick={() => console.log("heart clicked")} />
     </div>
