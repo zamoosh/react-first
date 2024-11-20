a8a554d3f1697972866ca01f47ecf2ae3a5915d1
Mohammad Rahimi <moyi.pary@gmail.com>
2024-11-20 20:14:04 +0330

    update the project, how to use the font awesome

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import "./assets/fontawesome-pro/css/regular.css";
+import "./assets/fontawesome-pro/css/solid.css";
+import "./assets/fontawesome-pro/css/fontawesome.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
