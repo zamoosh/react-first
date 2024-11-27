acdfea1404a14417320966cac314f8800a72e344
Mohammad Rahimi <moyi.pary@gmail.com>
2024-11-27 22:02:48 +0330

    could fetch data using axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const connect = () => console.log("Connecting");
-const disconnect = () => console.log("Disconnecting");
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+
   useEffect(() => {
-    connect();
-
-    return () => disconnect();
-  }, []);
-
-  return <div className={"container mt-3"}></div>;
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        setUsers([...res.data]);
+      });
+  }, [users]);
+  return (
+    <div className="container mt-3">
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">id</th>
+            <th scope="col">Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <th scope="row">{user.id}</th>
+              <td>{user.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
